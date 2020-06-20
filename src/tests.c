a697fb99573462ada221b0c91bafdc15b1002f0a
mlo055 <magnus_l@hotmail.no>
2020-06-20 18:02:36 +0200

    Add tests for horizontal and vertical rule

--- src/tests.c
+++ src/tests.c
@@ -2,16 +2,48 @@
 #include "CUnit/Basic.h"
 #include "game_logic.h"
 
+/* Tests the horizontal line (y) of the cell (x, y) */
+int horizontal_duplicates(int board[BOARD_SIZE][BOARD_SIZE], int x, int y) {
+    int i;
+   
+    for(i = 0; i < BOARD_SIZE; i++) {
+        if(board[i][y] == 0 || i == x) { /* Ignore zero and self */
+            i++;
+        }
+        if(board[i][y] == board[x][y]) {
+            return 1;
+        }
+    }
+    return 0;
+}
+
+/* Tests the vertical line (x) of the cell (x, y) */
+int vertical_duplicates(int board[BOARD_SIZE][BOARD_SIZE], int x, int y) {
+    int i;
+   
+    for(i = 0; i < BOARD_SIZE; i++) {
+        if(board[x][i] == 0 || i == y) { /* Ignore zero and self */
+            i++;
+        }
+        if(board[x][i] == board[x][y]) {
+            return 1;
+        }
+    }
+    return 0;
+}
 void test_generate_board(void) {
     int i, j;
     int board[BOARD_SIZE][BOARD_SIZE];
 
     generate_board(board);
 
+    /* Tests valid numbers, and board rules */
     for(i = 0; i < 9; i++) {
         for(j = 0; j < 9; j++) {
             CU_ASSERT(board[i][j] >= 0);
             CU_ASSERT(board[i][j] <= 9);
+            CU_ASSERT_FALSE(horizontal_duplicates(board, i, j));
+            CU_ASSERT_FALSE(vertical_duplicates(board, i, j));
         }
     }
 }
