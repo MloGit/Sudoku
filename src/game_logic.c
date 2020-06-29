c225fdb4562effa878d6f749674992af936b0cb0
mlo055 <magnus_l@hotmail.no>
2020-06-29 13:45:49 +0200

    Add empty generator and solver functions

---
+++ src/game_logic.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "game_logic.h"
+#include "helper_functions.h"
+
+// Generates a sudoku board, empty cells contains zero.
+void generate_board(int board[BOARD_SIZE][BOARD_SIZE]) {
+    return -1;
+}
+
+// Solves a given sudoku board
+int solve_board(int board[BOARD_SIZE][BOARD_SIZE]) {
+    return -1;
+}
