38ba01eecbf780043faa5a1b74ea636479eb979c
mlo055 <magnus_l@hotmail.no>
2020-07-11 17:08:42 +0200

    Add invalid test for check_board()

--- tests/test/test_check_board.c
+++ tests/test/test_check_board.c
@@ -15,9 +15,27 @@ void test_valid_check_board(void) {
     board[board_index(4, 4)] = 5;
     board[board_index(8, 8)] = 1;
 
-    check_cell_ExpectAndReturn(board, 0, 0, 0);
-    check_cell_ExpectAndReturn(board, 4, 4, 0);
-    check_cell_ExpectAndReturn(board, 8, 8, 0);
-
     TEST_ASSERT(check_board(board) == 0);
+}
+
+void test_invalid_check_board(void) {
+    int board[LINE * LINE] = {0};
+
+    check_cell_ExpectAndReturn(board, 0, 0, -1);
+    check_cell_ExpectAndReturn(board, 4, 4, -1);
+    check_cell_ExpectAndReturn(board, 8, 8, -1);
+
+    board[board_index(0, 0)] = 9;
+
+    TEST_ASSERT(check_board(board) == -1);
+
+    board[board_index(0, 0)] = 0;
+    board[board_index(4, 4)] = 5;
+
+    TEST_ASSERT(check_board(board) == -1);
+
+    board[board_index(4, 4)] = 0;
+    board[board_index(8, 8)] = 1;
+
+    TEST_ASSERT(check_board(board) == -1);
 }
