bd22ec11f1ebcab67c15b20414c321929d0e3d3e
mlo055 <magnus_l@hotmail.no>
2020-07-12 15:40:30 +0200

    Reimplement generate_board() test with mocking

---
+++ tests/test/test_generator.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "unity.h"
+#include "generator.h"
+#include "config.h"
+#include "helper_functions.h"
+#include "mock_solver.h"
+#include "mock_unique_solution.h"
+
+void setUp(void) {
+    init_generator();
+}
+
+void test_generate_board(void) {
+    int board[BOARD_SIZE];
+    int zero_board[BOARD_SIZE] = {0};
+
+    unique_solution_IgnoreAndReturn(0);
+
+    solve_board_ExpectAndReturn(board, 0, 0);
+    generate_board(board, 0);
+    TEST_ASSERT_NOT_EMPTY(board);
+
+    solve_board_ExpectAndReturn(board, 0, 0);
+    generate_board(board, LINE * 3); // Function fills, and empties 3 diagonal 3*3 boxes
+    TEST_ASSERT_EMPTY(board);
+}
