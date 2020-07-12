36445396b841fbe338f4b3fe8917a3ae2d89f11f
mlo055 <magnus_l@hotmail.no>
2020-07-12 16:42:15 +0200

    Add integration test of unique_solution()
    Uses the old dependent tests for unique_solution() as integration
    tests.

---
+++ tests/integration_tests/integration_test_unique_solution.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include "../vendor/ceedling/vendor/unity/src/unity.h"
+#include "../../src/unique_solution.h"
+#include "../../src/helper_functions.h"
+#include "../../src/config.h"
+
+void setUp(void) {
+}
+
+void tearDown(void) {
+}
+
+void test_valid_unique_solution(void) {
+    int board[BOARD_SIZE];
+    int rc;
+
+    rc = read_board_file(board, "../../testing_boards/s03a.txt");
+    TEST_ASSERT(rc == 0);
+
+    TEST_ASSERT(unique_solution(board) == 0);
+}
+
+void test_invalid_unique_solution(void) {
+    int board[BOARD_SIZE] = {0};
+
+    TEST_ASSERT(unique_solution(board) == -1);
+}
+
+void main(void) {
+    UNITY_BEGIN();
+    RUN_TEST(test_valid_unique_solution);
+    RUN_TEST(test_invalid_unique_solution);
+    UNITY_END();
+}
