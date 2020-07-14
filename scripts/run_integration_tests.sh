91fd266b3f109d2fe5671ede34ea319db616c610
mlo055 <magnus_l@hotmail.no>
2020-07-14 13:17:20 +0200

    Add run scripts

---
+++ scripts/run_integration_tests.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+function wait_for_user() {
+    echo ""
+    read -p "Press any key to continue... (or q to quit)" -n1 -s status
+    if [[ $status == "q" ]]; then
+        exit
+    fi
+    echo ""
+    echo ""
+}
+
+cd "$(dirname "$0")" # Set cwd to /scripts
+cd ../tests/integration_tests
+
+echo "These tests are hierarchical, so if the top one fails then the lower ones
+will fail or give false positives."
+
+wait_for_user
+
+gcc -o integration_test_solver integration_test_solver.c \
+    ../../src/solver.c \
+    ../../src/check_board.c \
+    ../../src/check_cell.c \
+    ../../src/helper_functions.c \
+    ../vendor/ceedling/vendor/unity/src/unity.c
+
+./integration_test_solver
+rm integration_test_solver
+
+wait_for_user
+
+gcc -o integration_test_unique_solution integration_test_unique_solution.c \
+    ../../src/unique_solution.c \
+    ../../src/compare_boards.c \
+    ../../src/solver.c \
+    ../../src/check_board.c \
+    ../../src/check_cell.c \
+    ../../src/helper_functions.c \
+    ../vendor/ceedling/vendor/unity/src/unity.c
+
+./integration_test_unique_solution
+rm integration_test_unique_solution
+
+wait_for_user
+
+gcc -o integration_test_generator integration_test_generator.c \
+    ../../src/generator.c \
+    ../../src/unique_solution.c \
+    ../../src/compare_boards.c \
+    ../../src/solver.c \
+    ../../src/check_board.c \
+    ../../src/check_cell.c \
+    ../../src/helper_functions.c \
+    ../vendor/ceedling/vendor/unity/src/unity.c
+
+./integration_test_generator
+rm integration_test_generator
---
+++ scripts/run_unit_tests.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+cd "$(dirname "$0")" # Set cwd to /scripts
+cd ../tests/
+
+ceedling test:all
