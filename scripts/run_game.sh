8ad99076067ba42f9bf34dacf110c009196a21f0
mlo055 <magnus_l@hotmail.no>
2020-07-15 19:21:45 +0200

    Implement main for game in terminal

---
+++ scripts/run_game.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+cd "$(dirname "$0")" # Set cwd to /scripts
+cd ../src/
+
+gcc -o main main.c \
+    player.c \
+    solver.c \
+    unique_solution.c \
+    generator.c \
+    check_cell.c \
+    check_board.c \
+    compare_boards.c \
+    helper_functions.c \
+
+./main
+rm main
--- src/main.c
+++ src/main.c
@@ -1,6 +1,32 @@
 #include <stdio.h>
-#include "game_logic.h"
+#include <string.h>
+#include "config.h"
+#include "player.h"
+#include "generator.h"
+#include "helper_functions.h"
 
 void main() {
-    printf("Hello world!\n");
+    /*
+     * In loop:
+     * Get input
+     * Do command
+     */
+
+    int board[BOARD_SIZE] = {0};
+    int start_board[BOARD_SIZE] = {0};
+    char command[7];
+    int run = 0;
+
+    init_generator();    
+    do_command("new", board, start_board);
+
+    printf("Sudoku game\n");
+    printf("Waiting for input ('enter' to modify cell, 'help', or 'quit')\n");
+
+    while(run == 0) {
+        printf(">>> ");
+        fgets(command, sizeof(command), stdin);
+        command[strcspn(command, "\n")] = '\0';
+        run = do_command(command, board, start_board);
+    }
 }
